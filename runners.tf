ae691aa0115b9611e5559449e29c61300c9129af
tjtharrison <gh@tjth.co>
2022-06-19 20:48:14 +0100

    Fixed runner path

--- runners.tf
+++ runners.tf
@@ -1,6 +1,6 @@
 module "tjth_runner" {
     # source = "git@github.com:tjtharrison/tjth-github-runners.git//modules/runner?ref=1.0.0"
-    source = "../modules/runner"
+    source = "./modules/runner"
     runner_name = "default"
     github_team_url = "https://github.com/tjtharrison/tcoin"
 }
